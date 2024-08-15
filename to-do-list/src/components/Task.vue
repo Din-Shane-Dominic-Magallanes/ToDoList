dec3e398374db946cd6c66b01378091ea276a370
DinShaneDominicMagallanes <dinmagallanes123@gmail.com>
2024-08-15 13:47:47 +0800

    Can delete and add only

--- to-do-list/src/components/Task.vue
+++ to-do-list/src/components/Task.vue
@@ -7,7 +7,7 @@
         </div>
         <!-- Search -->
         <div class="search">
-          <input type="text" placeholder="Search Task" />
+          <input type="text" placeholder="Search Task" @input="tasks = searchTodo" />
           <button><font-awesome-icon :icon="['fas', 'search']" /></button>
         </div>
         <!-- Task lists -->
@@ -16,17 +16,17 @@
             <li v-for="task in tasks" :key="task.id" :class="{'toggle-completed': task.completed}">
               <button @click="toggle(task)"><font-awesome-icon :icon="task.completed ? ['fas', 'check-circle'] : ['far', 'circle']" /></button>
               <button @click="toggle(task)">{{ task.title }}</button>
-              <button><font-awesome-icon :icon="['far', 'trash-alt']" /></button>
+              <button @click="deleteTodo(task.id)"><font-awesome-icon :icon="['far', 'trash-alt']" /></button>
             </li>
           </ul>
         </div>
         <!-- Form -->
          <div class="form">
-          <input type="text" placeholder="New Task" />
-          <button><font-awesome-icon :icon="['fas', 'plus']" /></button>
+          <input v-model="newTask" type="text" placeholder="New Task" @keyup.enter="addTodo" />
+          <button @click="addTodo"><font-awesome-icon :icon="['fas', 'plus']" /></button>
         </div>
         <!-- Filters -->
-        <div class="clearBtns">
+        <div class="filterBtns">
           <button>Not Completed</button>
           <button>Completed</button>
         </div>
@@ -49,8 +49,28 @@
   },
 
   methods: {
-    addTodo(){
-    
+
+    searchTodo(event) {
+      const searchTerm = event.target.value.toLowerCase();
+      return this.tasks.filter(task => task.title.toLowerCase().includes(searchTerm));
+    }
+
+    ,addTodo(){
+      if (this.newTask.trim()) {
+        this.tasks.push({
+          id: this.tasks.length + 1,
+          title: this.newTask,
+          completed: false,
+        })
+        this.newTask = "";
+      }
+    }
+
+    ,deleteTodo(taskId) {
+      const index = this.tasks.findIndex(task => task.id === taskId);
+      if (index !== -1) {
+        this.tasks.splice(index, 1);
+      }
     }
 
     ,toggle(task){
