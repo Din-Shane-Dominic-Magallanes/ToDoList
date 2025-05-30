65e3dcb90078aa7d6188408b042acdb96b831b3a
DinShaneDominicMagallanes <dinmagallanes123@gmail.com>
2025-05-31 06:18:58 +0800

    Added QR code plugin for local hosting

--- to-do-list/vite.config.js
+++ to-do-list/vite.config.js
@@ -1,12 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import { qrcode } from 'vite-plugin-qrcode';
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    qrcode() 
   ],
   resolve: {
     alias: {
